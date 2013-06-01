6efc39cb82932074bb760a1faac85447c791d13b
J <jamie@offtherails.net.nz>
2013-06-01 15:22:45 +1000

    Update create_coverage_files.py

--- create_coverage_files.py
+++ create_coverage_files.py
@@ -13,12 +13,14 @@ def create(file_name):
 	
 	#make opaque here
 
-	#White to transparent
-	w2t = 'convert -transparent "#FFFFFF" ' + file_name + '.ppm ' + file_name +'_trans.ppm'
 
 	#Convert ppm to png
-	p2p = 'pnmtopng ' + file_name + '_trans.ppm ' + file_name + '.png' 
+	p2p = 'pnmtopng ' + file_name + ' > ' + file_name + '.png' 
 	print p2p
+
+	#White to transparent
+	w2t = 'convert -transparent "#FFFFFF" ' + file_name + '.png ' + file_name +'.png'
+
 
 	#remove trans file
 	rm_trans = 'rm ' + file_name +'_trans.ppm'
@@ -35,18 +37,21 @@ def create(file_name):
 		print 'Coverage map created'
 	except:
 		print 'Coverage map failed...'	
-	try:
-		print 'Converting white to transparency'
-		os.system(w2t)
-		print 'Converted white to transparency'
-	except:
+
 		'Converting white to transparency failed' 
 	try:
 		print 'Converting ppm to png'
 		os.system(p2p)
 		print 'Converted ppm to png'
+
 	except:
 		print 'Failed converting ppm to png'
+	try:
+		print 'Converting white to transparency'
+		os.system(w2t)
+		print 'Converted white to transparency'
+	except:
+			
 	try:
 		print 'Cleaning file'
 		os.system(rm_trans)
