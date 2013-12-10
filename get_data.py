8c22fc8eff59399f63d190f14132e7028bc094ab
Jamie <jamie@offtherails.net.nz>
2013-12-11 10:58:15 +1300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,5 +1,4 @@
 '''Parses the DEM data, and grabs each zip file from the web page'''
-
 import httplib2
 import urllib
 
@@ -7,22 +6,23 @@ from BeautifulSoup import BeautifulSoup, SoupStrainer
 
 counter = 0
 
-query_url = 'http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Australia/'
+# query_url = 'http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Australia/'
+query_url = 'http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Islands/'
 
 http = httplib2.Http()
 status, response = http.request(query_url)
 
 
 for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
-  try:
-		if link.has_key('href'):
-			suffix = link['href']
-			#The following allows you to limit to a region by setting lat/long
-			# if (int(suffix[1:3]) > 29) and (int(suffix[-11:-8])> 141) == True:
-			urllib.urlretrieve(query_url + "/" + suffix, filename =  suffix )
-			print 'Success with: ' + suffix
-			counter = counter + 1
-	except:
-		continue
+    try:
+        if link.has_key('href'):
+            suffix = link['href']
+            # The following allows you to limit to a region by setting lat/long
+            if (int(suffix[1:3]) > 34) and (int(suffix[-11:-8])> 160) == True:
+                urllib.urlretrieve(query_url + "/" + suffix, filename =  suffix )
+                print 'Success with: ' + suffix
+                counter = counter + 1
+    except:
+            continue
 
-print counter + ' files downloaded successfully'
+print str(counter) + ' files downloaded successfully'
